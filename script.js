87172ec3e3ae98024a7fb70fc15c72a91a4181b2
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-09-12 10:42:22 +0530

    camera filters feature added

--- script.js
+++ script.js
@@ -14,26 +14,26 @@ let constraints = {
 
 let chunks = [];
 
-// navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-//     video.srcObject = stream
-//     recorder = new MediaRecorder(stream);
-//
-//     recorder.addEventListener("start", (e) => {
-//         chunks = [];
-//     })
-//     recorder.addEventListener("dataavailable", (e) => {
-//         chunks.push(e.data);
-//     })
-//     recorder.addEventListener("stop" , (e) => {
-//         // conversion of media chunks to video
-//         let blob = new Blob(chunks, {type: "video/mp4"});
-//         let videoURL = URL.createObjectURL(blob);
-//         let a = document.createElement('a');
-//         a.href = videoURL;
-//         a.download = "stream.mp4";
-//         a.click()
-//     })
-// })
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream
+    recorder = new MediaRecorder(stream);
+
+    recorder.addEventListener("start", (e) => {
+        chunks = [];
+    })
+    recorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data);
+    })
+    recorder.addEventListener("stop" , (e) => {
+        // conversion of media chunks to video
+        let blob = new Blob(chunks, {type: "video/mp4"});
+        let videoURL = URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        a.href = videoURL;
+        a.download = "stream.mp4";
+        a.click()
+    })
+})
 
 recordBtnContainer.addEventListener('click', (e) => {
     if(!recorder) return;
